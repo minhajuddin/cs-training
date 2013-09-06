3c585d5df03dd360d18b16319c8b7d7db74571fe
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2013-09-06 13:00:31 +0530

    Ability to view contacts

--- ruby/solutions/contact-manager/contact-manager.rb
+++ ruby/solutions/contact-manager/contact-manager.rb
@@ -10,11 +10,30 @@ def add_contact
   #address = gets.chomp.strip
 
   f = File.open('contacts.txt', 'a+')
-  f.write "#{name} #{email}\n"
+  f.write "#{name}|#{email}\n"
   #f.write "#{name} #{email} #{phone} #{address}"
   f.close
 end
 
 while true
-  add_contact
+  puts "1. Add Contact\n2. List all contacts"
+  option = gets.chomp.strip.to_i
+  if option == 1
+    add_contact
+  elsif option == 2
+    puts "Contacts\n==========="
+    contacts = File.read('contacts.txt').split("\n")
+    i = 1
+    contacts.each do |contact|
+      contact_tokens = contact.split('|')
+      name = contact_tokens[0]
+      email = contact_tokens[1]
+      puts "#{i}."
+      puts "Name: #{name}"
+      puts "Email: #{email}"
+      puts '----------'
+      i += 1
+    end
+    puts "==========="
+  end
 end
